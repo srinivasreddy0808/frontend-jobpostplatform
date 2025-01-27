60b8c04e865ae8e38aea270147c5477cec40f032
srinivasreddy0808 <srinivasreddybrockk@gmail.com>
2025-01-27 21:16:40 +0530

    Login.jsx changed

--- src/features/authentication/Login.jsx
+++ src/features/authentication/Login.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/authcontext";
+import { AuthContext } from "../../context/authContext";
 import styles from "./Login.module.css";
 
 const Login = () => {
